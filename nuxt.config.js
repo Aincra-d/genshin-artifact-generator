dfc0d910177913d96e10ee3fea62797faf4f690a
Aincra-d <kisspatya2@gmail.com>
2021-03-15 12:32:13 +0100

    Changed PWA settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ export default {
 
   pwa: {
     icon: {
-        source: 'app-icon.png',
+        // source: 'https://genshin-artifacts.netlify.app/icon.png',
         fileName: 'app-icon.png',
         sizes: [64, 120, 144, 152, 192, 384, 512]
     },
