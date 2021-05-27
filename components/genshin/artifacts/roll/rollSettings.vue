8b5320bccc78f2573eba52838413bfa078c99160
Aincra-d <kisspatya2@gmail.com>
2021-05-27 20:24:47 +0200

    Changed tooltips on roll settings

--- components/genshin/artifacts/roll/rollSettings.vue
+++ components/genshin/artifacts/roll/rollSettings.vue
@@ -5,52 +5,60 @@
         text="Artifact roll settings"
         variant="outline-light"
         class="text-dark rounded-0 border-0 outline-0 mx-auto">
-            <b-dropdown-item
-            v-tooltip="{content: 'Roll 1-3 star artifacts too when rolling', delay: {show:250}, hideOnTargetClick: true}"
-            @click.native.capture.stop="setRollSettings('include_low_stars')">
+            <b-dropdown-item @click.native.capture.stop="setRollSettings('include_low_stars')">
                 <i
                 class="fa-sm"
                 :class="roll_settings.include_low_stars ?
                 'fas fa-check-square' : 'far fa-square'">
                 </i>
 
-                Include 1-3 star
+                <span>Include 1-3 star</span>
+
+                <i
+	            v-tooltip="{content: 'Roll 1-3 star artifacts too when rolling', delay: {show:250}, hideOnTargetClick: true}"
+	            class="fas fa-question-circle fa-sm d-inline-block"></i>
             </b-dropdown-item>
 
-            <b-dropdown-item
-            v-tooltip="{content: 'Only do one upgrade at a time, no option to choose levels', delay: {show:250}, hideOnTargetClick: true}"
-            @click.native.capture.stop="setRollSettings('single_upgrades')">
+            <b-dropdown-item @click.native.capture.stop="setRollSettings('single_upgrades')">
                 <i
                 class="fa-sm"
                 :class="roll_settings.single_upgrades ?
                 'fas fa-check-square' : 'far fa-square'">
                 </i>
                 
-                Single upgrades
+                <span>Single upgrades</span>
+
+                <i
+            	v-tooltip="{content: 'Only do one upgrade at a time, no option to choose levels', delay: {show:250}, hideOnTargetClick: true}"
+	            class="fas fa-question-circle fa-sm d-inline-block"></i>
             </b-dropdown-item>
 
-            <b-dropdown-item
-            v-tooltip="{content: 'Show upgrade results in a window', delay: {show:250}, hideOnTargetClick: true}"
-            @click.native.capture.stop="setRollSettings('show_upgrades')">
+            <b-dropdown-item @click.native.capture.stop="setRollSettings('show_upgrades')">
                 <i
                 class="fa-sm"
                 :class="roll_settings.show_upgrades ?
                 'fas fa-check-square' : 'far fa-square'">
                 </i>
                 
-                Show upgrades
+                <span>Show upgrades</span>
+
+                <i
+            	v-tooltip="{content: 'Show upgrade results in a window', delay: {show:250}, hideOnTargetClick: true}"
+	            class="fas fa-question-circle fa-sm d-inline-block"></i>
             </b-dropdown-item>
 
-            <b-dropdown-item
-            v-tooltip="{content: 'Only do 10x artifact rolls', delay: {show:250}, hideOnTargetClick: true}"
-            @click.native.capture.stop="setRollSettings('roll_10x')">
+            <b-dropdown-item @click.native.capture.stop="setRollSettings('roll_10x')">
                 <i
                 class="fa-sm"
                 :class="roll_settings.roll_10x ?
                 'fas fa-check-square' : 'far fa-square'">
                 </i>
                 
-                10x rolls
+                <span>10x rolls</span>
+
+                <i
+                v-tooltip="{content: 'Only do 10x artifact rolls', delay: {show:250}, hideOnTargetClick: true}"
+	            class="fas fa-question-circle fa-sm d-inline-block"></i>
             </b-dropdown-item>
         </b-dropdown>
 	</div>
