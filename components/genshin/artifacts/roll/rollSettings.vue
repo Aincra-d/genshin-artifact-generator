3384315438ac8ab1fae89f8ffc103bf939b646db
Aincra-d <kisspatya2@gmail.com>
2021-06-23 11:37:34 +0200

    Fixed settings tooltip positions

--- components/genshin/artifacts/roll/rollSettings.vue
+++ components/genshin/artifacts/roll/rollSettings.vue
@@ -5,7 +5,7 @@
         text="General site settings"
         variant="outline-light"
         class="text-dark rounded-0 border-0 outline-0 mx-auto">
-            <b-dropdown-item @click.native.capture.stop="setSettings('include_low_stars')">
+            <b-dropdown-item class="position-relative" @click.native.capture.stop="setSettings('include_low_stars')">
                 <i
                 class="fa-sm"
                 :class="settings.include_low_stars ?
@@ -16,10 +16,10 @@
 
                 <i
 	            v-tooltip="{content: 'Roll 1-3 star artifacts too when rolling', delay: {show:250}, hideOnTargetClick: true}"
-	            class="fas fa-question-circle fa-sm d-inline-block"></i>
+	            class="fas fa-question-circle position-absolute top-30 right-2 fa-sm d-inline-block"></i>
             </b-dropdown-item>
 
-            <b-dropdown-item @click.native.capture.stop="setSettings('single_upgrades')">
+            <b-dropdown-item class="position-relative" @click.native.capture.stop="setSettings('single_upgrades')">
                 <i
                 class="fa-sm"
                 :class="settings.single_upgrades ?
@@ -30,10 +30,10 @@
 
                 <i
             	v-tooltip="{content: 'Only do one upgrade at a time, no option to choose levels', delay: {show:250}, hideOnTargetClick: true}"
-	            class="fas fa-question-circle fa-sm d-inline-block"></i>
+	            class="fas fa-question-circle position-absolute top-30 right-2 fa-sm d-inline-block"></i>
             </b-dropdown-item>
 
-            <b-dropdown-item @click.native.capture.stop="setSettings('show_upgrades')">
+            <b-dropdown-item class="position-relative" @click.native.capture.stop="setSettings('show_upgrades')">
                 <i
                 class="fa-sm"
                 :class="settings.show_upgrades ?
@@ -44,10 +44,10 @@
 
                 <i
             	v-tooltip="{content: 'Show upgrade results in a window', delay: {show:250}, hideOnTargetClick: true}"
-	            class="fas fa-question-circle fa-sm d-inline-block"></i>
+	            class="fas fa-question-circle position-absolute top-30 right-2 fa-sm d-inline-block"></i>
             </b-dropdown-item>
 
-            <b-dropdown-item @click.native.capture.stop="setSettings('roll_10x')">
+            <b-dropdown-item class="position-relative" @click.native.capture.stop="setSettings('roll_10x')">
                 <i
                 class="fa-sm"
                 :class="settings.roll_10x ?
@@ -58,10 +58,10 @@
 
                 <i
                 v-tooltip="{content: 'Only do 10x artifact rolls', delay: {show:250}, hideOnTargetClick: true}"
-	            class="fas fa-question-circle fa-sm d-inline-block"></i>
+	            class="fas fa-question-circle position-absolute top-30 right-2 fa-sm d-inline-block"></i>
             </b-dropdown-item>
 
-            <b-dropdown-item @click.native.capture.stop="setSettings('show_set_effects')">
+            <b-dropdown-item class="position-relative" @click.native.capture.stop="setSettings('show_set_effects')">
                 <i
                 class="fa-sm"
                 :class="settings.show_set_effects ?
@@ -72,7 +72,7 @@
 
                 <i
                 v-tooltip="{content: 'Show set effects by default', delay: {show:250}, hideOnTargetClick: true}"
-	            class="fas fa-question-circle fa-sm d-inline-block"></i>
+	            class="fas fa-question-circle position-absolute top-30 right-2 fa-sm d-inline-block"></i>
             </b-dropdown-item>
         </b-dropdown>
 	</div>
